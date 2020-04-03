ec854fa4f7e57b31578d0fe9a89f1ca3a1aae164
Bufod <nikolaybonar97@gmail.com>
2020-04-03 13:58:43 +0300

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button signInBt;
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
